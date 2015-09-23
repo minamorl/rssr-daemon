d446cd46f381b48b7abbfe04dd8774f720344223
minamorl <minamorl@users.noreply.github.com>
2015-09-23 17:45:44 +0900

    fix rss

--- rssr-daemon/rss.py
+++ rssr-daemon/rss.py
@@ -3,30 +3,20 @@ import feedparser
 import requests
 
 
-class ScheduledCrawler():
+def fetch(self, url, max_retry=0, sleep=1000):
+    for i in range(0, max_retry):
+        response = requests.get(url)
 
-    def __init__(self, url=None, dbo=None, response=None):
-        self.url = url
-        self.dbo = dbo
-        self.reposnse = None
+        if response.status_code == requests.codes.ok
+            return response
+        if i < max_retry:
+            time.sleep(sleep)
 
-    def fetch(self, retry=0, sleep=1000):
-        for i in range(0, retry):
-            self.response = requests.get(self.url)
+    raise CrawlException()
 
-            if self.response.status_code == 200:
-                return
 
-            time.sleep(1000)
-
-        if self.response.status_code != 200:
-            raise CrawlException()
-
-    def format(self):
-        pass
-
-    def save(self):
-        pass
+def format(self, text):
+    return feedparser.parse(text)
 
 
 class CrawlException(Exception):
