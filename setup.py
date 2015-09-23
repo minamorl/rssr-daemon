e0fa6e83f02db06802d0559915c1beab8fb3187d
minamorl <minamorl@users.noreply.github.com>
2015-09-23 17:47:20 +0900

    chore: fix entrypoint

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from setuptools import setup, find_packages
 
-app_name = "rssr-daemon"
-
 setup(
     name=appname,
     version="0.0.1",
     packages=find_packages(),
     entry_points={
-        'console_scripts': ['{} = {}.__main__:main'.format(appname)]
+        'console_scripts': ['rssr-daemon = rssr.__main__:main']
     },
 )
