032c5ca4253e42ede9c140a1018b2c4d93ae8ca6
minamorl <minamorl@users.noreply.github.com>
2015-09-23 18:04:01 +0900

    feat: add daemonize option

--- rssr/__main__.py
+++ rssr/__main__.py
@@ -1,6 +1,23 @@
-def main():
+import daemon
+import argparse 
+
+def _argparse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--daemonize', action='store_true')
+    return parser
+
+def _main():
     pass
 
+def main():
+    args = _argparse().parse_args()
+
+    if args.daemonize:
+        with daemon.DaemonContext():
+            _main()
+    else:
+        _main()
 
 if __name__ == '__main__':
     main()
+
