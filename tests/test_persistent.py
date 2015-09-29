e3962ef509f7779946fb677336cc9bab4150fb28
minamorl <minamorl@users.noreply.github.com>
2015-09-29 22:15:47 +0900

    feat: automate sample file detection by glob

--- tests/test_persistent.py
+++ tests/test_persistent.py
@@ -1,14 +1,16 @@
 import rssr.persistent
 import feedparser
 import os
+import glob
 
 
 _dir = os.path.abspath(os.path.dirname(__file__))
 
 def test_format():
-
-    with open(os.path.join(_dir, 'samples/sample_feed01.atom')) as data:
-        d = feedparser.parse(data.read()).entries
-        for item in d:
-            # Just execute to check "format" working collectly.
-            print(rssr.persistent.format(item))
+    
+    for filename in glob.glob(os.path.join(_dir, 'samples/sample_feed*')):
+        with open(filename) as data:
+            d = feedparser.parse(data.read()).entries
+            for item in d:
+                # Just execute to check "format" working collectly.
+                print(rssr.persistent.format(item))
