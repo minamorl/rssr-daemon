521c737da22d766951cb248df4dc52d1016118a9
minamorl <minamorl@users.noreply.github.com>
2015-11-15 00:17:21 +0900

    Close #3

--- rssr/persistent.py
+++ rssr/persistent.py
@@ -5,6 +5,8 @@ import feedparser
 import datetime
 from time import mktime
 from rssr.utils import _logger
+import redisorm.core
+import redisorm.proxy
 import sys
 
 logger = _logger(__name__)
@@ -20,26 +22,44 @@ def get_redis():
         sys.exit(1)
 
 
-def format(item):
-    formatted = {
-        "id": item.id,
-        "link": item.link,
-        "published": datetime.datetime.fromtimestamp(mktime(item.published_parsed)).strftime('%Y-%m-%d %H:%M:%S'),
-        "title": item.title,
-        "author": item.author,
-        "summary": item.summary, }
-    if "content" in item:
-        formatted.update({"content": item.content[0].value})
 
-    return formatted
+class Feed(redisorm.core.PersistentData):
+    def __init__(self, id=None, url=None):
+        self.id = id
+        self.url = url
 
+class FeedItem(redisorm.core.PersistentData):
+    def __init__(self, id=None, feed=None, link=None, published=None, title=None, author=None, summary=None, content=None):
+        self.id = id
+        self.feed = redisorm.proxy.PersistentProxy(feed)
+        self.link = link
+        self.published = published
+        self.title = title
+        self.author = author
+        self.summary = summary
+        self.content = content
+
+    @classmethod
+    def create_from(cls, feed, item):
+        obj = cls(feed=feed)
+        obj.link = item.link
+        obj.published = datetime.datetime.fromtimestamp(mktime(item.published_parsed)).strftime('%Y-%m-%d %H=%M=%S')
+        obj.title = item.title
+        obj.author = item.author
+        obj.summary = item.summary
+        if "content" in item:
+            obj.content= item.content[0].value
+        return obj
 
 def save_parsed_value(url, data, r=get_redis()):
+    p = redisorm.core.Persistent("rssr", r=r)
     d = feedparser.parse(data).entries
-    for item in d:
-        redis_key = "rssr:feed:{feed_url}:{item}".format(feed_url=url, item=item.link)
-        formatted = format(item)
-        r.hmset(redis_key, formatted)
+
+    feed = Feed(url)
+    p.save(feed)
+    for _item in d:
+        item = FeedItem.create_from(feed, _item)
+        p.save(item)
 
 
 def save_raw_feed(data, fp):
--- tests/test_persistent.py
+++ tests/test_persistent.py
@@ -1,16 +1,33 @@
-import rssr.persistent
+from rssr.persistent import FeedItem, Feed
 import feedparser
 import os
 import glob
+import pytest
+import redisorm.core
+import redis
 
 
 _dir = os.path.abspath(os.path.dirname(__file__))
 
-def test_format():
-    
+
+@pytest.fixture
+def test_redis():
+    port = os.environ.get("TEST_REDIS_PORT")
+    r = redis.StrictRedis(port=port, decode_responses=True)
+    r.ping()
+    r.flushdb()
+    return r
+
+
+def test_format_and_save(test_redis):
+    p = redisorm.core.Persistent("rssr", r=test_redis)
     for filename in glob.glob(os.path.join(_dir, 'samples/sample_feed*')):
+
         with open(filename) as data:
-            d = feedparser.parse(data.read()).entries
-            for item in d:
-                # Just execute to check "format" working collectly.
-                print(rssr.persistent.format(item))
+
+            feed = Feed(url=filename)
+            p.save(feed)
+
+            for item in feedparser.parse(data.read()).entries:
+                item = FeedItem.create_from(feed, item)
+                p.save(item)
